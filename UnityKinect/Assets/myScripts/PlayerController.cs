b9e91c62265b8b1519c7dd767adb7517f597a0ab
Alan Murphy <alanmurphy1995@gmail.com>
2017-03-26 15:43:42 +0100

    Added a basic UI

--- UnityKinect/Assets/myScripts/PlayerController.cs
+++ UnityKinect/Assets/myScripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	
 	public float speed;
+	public Text countText;
+	public Text winText;
 	private Rigidbody rb;
+	private int count;
 
 	void Start (){
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate(){
@@ -23,6 +30,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			setCountText ();
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count:  " + count.ToString ();
+		if (count >= 9) {
+			winText.text = "You Win!";
 		}
 	}
 }
