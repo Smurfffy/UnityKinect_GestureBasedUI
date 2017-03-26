f55f316e5cbc6c6aa1ed1b524b611f3c99aef82d
Alan Murphy <alanmurphy1995@gmail.com>
2017-03-26 15:23:07 +0100

    Made pickup objects collectable

--- UnityKinect/Assets/myScripts/PlayerController.cs
+++ UnityKinect/Assets/myScripts/PlayerController.cs
@@ -19,4 +19,10 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
 }
