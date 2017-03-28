0f7eb9768e84be13b88fc1cff39fbba4ab7d6800
Alan Murphy <alanmurphy1995@gmail.com>
2017-03-28 17:10:26 +0100

    Added Kinect controll with the object

---
+++ UnityKinectMaze/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveForward : MonoBehaviour {
+
+	public float forwardspeed = 2.0f;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * forwardspeed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			setCountText ();
+			if (count >= 6) {
+				winText.text = "You win!";
+			}
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count: " + count.ToString ();
+	}
+}
---
+++ UnityKinectMaze/Assets/Scripts/Turning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turning : MonoBehaviour {
+	public bool turnLeft;
+	public bool turnRight;
+	private bool rotating;
+	public float rotatespeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (turnLeft) {
+			transform.Rotate (new Vector3 (0, -20, 0) * Time.deltaTime);
+		}
+		if (turnRight) {
+			transform.Rotate (new Vector3 (0, 20, 0) * Time.deltaTime);
+		}
+	}
+}
